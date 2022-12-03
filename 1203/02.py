6208fab87bff9b8d3d366d24b6822e8fa7a4a421
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-03 15:15:26 +0800

    update 02

--- 1203/02.py
+++ 1203/02.py
@@ -1,11 +1,22 @@
 import turtle
 
 
+def nb():
+    """
+    this program is super cool
+    this program is made in china
+
+
+    """
+    print("supercool")
+
+
 def main():
     t = turtle.Pen()
     for x in range(100):
         t.forward(x)
         t.left(72)
+        nb()
 
 
 if __name__ == '__main__':
