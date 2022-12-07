6acc5557e3a95500de8db1b19313a533a45f29ef
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-07 11:20:55 +0800

    2

--- 1206/difference.py
+++ 1206/difference.py
@@ -1,7 +1,14 @@
 def main():
     c = "ee#"
-    f = "ee#"
-    print(id(c) == id(f))
+    d = "ee#"
+    print(id(c) == id(d))
+    # idle(交互模式)里驻留的标识符仅有下划线，字母数字，带有标识符的字符串有驻留机制，第一个输出与idle不同
+    e = "ee_"
+    f = "ee_"
+    print(id(e) == id(f))
+    g = ""
+    h = ""
+    print(id(g) == id(h))
 
 
 if __name__ == '__main__':
