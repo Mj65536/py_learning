63bfe14d44a5577cea2828006b444ead61b4fc08
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-07 11:33:51 +0800

    2

---
+++ 1207/UPPER & lower.py
@@ -0,0 +1,11 @@
+def main():
+    a = "Maji, mAJI"
+    print(a.capitalize())  # 仅大写字符串中第一个字母
+    print(a.title())       # 每个单词首字母大写
+    print(a.upper())       # 全部变成大写字母
+    print(a.lower())       # 全部变成小写字母
+    print(a.swapcase())    # 交换大小写
+
+
+if __name__ == '__main__':
+    main()
