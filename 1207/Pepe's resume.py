b407c5e7b4a810050c6c19fde82ab6401c3c7bc3
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-07 15:48:06 +0800

    o

--- 1207/Pepe's resume.py
+++ 1207/Pepe's resume.py
@@ -1,6 +1,8 @@
 def main():
     a = 'My name is {a}, my age is {b}, my signature phrase is {b}{b}{b}.'
     print(a.format(a='Pepe', b='6'))
+    b = "{name} have {money:+.4f}$ in his credit card."
+    print(b.format(name="Pepe", money=9999999.99999))
 
 
 if __name__ == '__main__':
