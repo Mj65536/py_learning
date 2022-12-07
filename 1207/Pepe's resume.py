cfc351a65c7df074f8e19aab0d40e46984d4ddac
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-07 15:34:11 +0800

    o

---
+++ 1207/Pepe's resume.py
@@ -0,0 +1,7 @@
+def main():
+    a = 'My name is {a}, my age is {b}, my signature phrase is {b}{b}{b}.'
+    print(a.format(a='Pepe', b='6'))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 1207/拍板.py
@@ -0,0 +1,9 @@
+def main():
+    a = " PeP "
+    print(a.center(7, '$'))
+    print(a.rjust(7, '%'))
+    print(a.ljust(7, '%'))
+
+
+if __name__ == '__main__':
+    main()
