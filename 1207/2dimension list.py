f99304f11f8885afb41e39d0e6ac30b9d6042d37
mj65536 <119717335+Mj65536@users.noreply.github.com>
2022-12-08 17:20:06 +0800

    12.08

--- 1207/2dimension list.py
+++ 1207/2dimension list.py
@@ -1,24 +1,32 @@
 def main():
-    a = [
-            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-            [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-            [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-            [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
-        ]
-
+    # a = [
+    #    [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    #    [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+    #    [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+    #    [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+    # ]
+    a = list(range(11))
     # a.extend(list(range(11, 21)))
     # b = [i**4 for i in range(1, 11)]
     # print(a.count(5))
-    # c = a.sort(reverse=True)
-    # print(c)
-    # print(a.reverse())
-    # print(a)
+    c = [16, 1, 5, 9, 6, 7]
+    c.sort()
+    print(c)
+    # print(type(c))
     # print(a.sort(reverse=True))
-    for x in range(3):
-        for y in range(10):
-            print(a[x][y], end=' ')
-        print('\n')
-    print("打印完成")
+    # for x in range(3):
+    #    for y in range(10):
+    #        print(a[x][y], end='  ')
+    #    print('\n')
+    # print("打印完成")
+
+    # b = tuple('6745678987456789098765431247890865')
+    # print(sorted(b))
+    # j = [1, 2, 3]
+    # m = [4, 5, 6]
+    # d = zip(j, m)
+    # print(d)
+    # list(d)
 
 
 if __name__ == '__main__':
