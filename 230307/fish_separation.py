70a4952058e173183785456d661424a53d63c51a
mj65536 <119717335+Mj65536@users.noreply.github.com>
2023-03-07 16:04:06 +0800

    230307

---
+++ 230307/fish_separation.py
@@ -0,0 +1,26 @@
+def main():
+    i = 1
+    def calc():
+        i += 1
+        n = 5 * i + 1
+        a1 = (n - 1) / 5
+        a2 = (n - a1 - 2) / 5
+        a3 = (n - a1 - a2 - 3) / 5
+        a4 = (n - a1 - a2 - a3 - 4) / 5
+        a5 = (n - a1 - a2 - a3 - a4 - 5) / 5
+        return i, n, a1, a2, a3, a4, a5
+
+        while 1:
+            if not (a5 * 5) % 5 == 0:
+                calc()
+            else:
+                print(f'第一个人醒来后看到{n}条鱼')
+                print(f'第二个人醒来后看到{int(a2 * 5 + 1)}条鱼')
+                print(f'第三个人醒来后看到{int(a3 * 5 + 1)}条鱼')
+                print(f'第四个人醒来后看到{int(a4 * 5 + 1)}条鱼')
+                print(f'第五个人醒来后看到{int(a5 * 5 + 1)}条鱼')
+                break
+
+
+if __name__ == '__main__':
+    main()
